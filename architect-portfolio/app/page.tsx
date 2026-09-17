4b7c99b0db9cc5865eaeaf4c65999c08f4f3aaf1
BENBEDRAMED <ma.benbedra@esi-sba.dz>
2026-09-17 22:52:01 +0100

    maybe it work 4

--- architect-portfolio/app/page.tsx
+++ architect-portfolio/app/page.tsx
@@ -1,15 +1,12 @@
-// import Navbar from "@/components/Navbar";
-// import Hero from "@/components/Hero";
-// import { motion } from "framer-motion";
-// export default function Home() {
-//   return (
-//     <main className="w-full">
-//       <Navbar />
-//       <Hero />
-//     </main>
-//   );
-// }
-
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import { motion } from "framer-motion";
 export default function Home() {
-  return <h1 style={{ color: "white" }}>HOME OK</h1>;
+  return (
+    <main className="w-full">
+      <Navbar />
+      <Hero />
+    </main>
+  );
 }
+

1e0e591f9f6e905db6698190de50b0af20182483
BENBEDRAMED <ma.benbedra@esi-sba.dz>
2026-09-17 22:49:36 +0100

    maybe it work 3

--- architect-portfolio/app/page.tsx
+++ architect-portfolio/app/page.tsx
@@ -1,11 +1,15 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import { motion } from "framer-motion";
+// import Navbar from "@/components/Navbar";
+// import Hero from "@/components/Hero";
+// import { motion } from "framer-motion";
+// export default function Home() {
+//   return (
+//     <main className="w-full">
+//       <Navbar />
+//       <Hero />
+//     </main>
+//   );
+// }
+
 export default function Home() {
-  return (
-    <main className="w-full">
-      <Navbar />
-      <Hero />
-    </main>
-  );
+  return <h1 style={{ color: "white" }}>HOME OK</h1>;
 }

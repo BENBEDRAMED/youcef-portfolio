230a6082ab3021f4253d82ec5ae290a036e45cc4
BENBEDRAMED <ma.benbedra@esi-sba.dz>
2026-09-19 19:36:42 +0100

    fixing loading projecct background

--- architect-portfolio/components/Projects.tsx
+++ architect-portfolio/components/Projects.tsx
@@ -30,7 +30,7 @@ export default function ProjectsPageHero() {
         className="fixed inset-0 -z-10 w-full h-full scale-110 pointer-events-none"
       >
         <Image
-          src="/meusum.jfif"
+          src="/meusum.png"
           alt="Projects Background"
           fill
           priority
